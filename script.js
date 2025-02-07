bfeb0817e5212c6dff94ee04b61eb94d03758cbd
Akash Kumar Tiwary <128840791+AkashKT10172@users.noreply.github.com>
2025-02-08 00:41:10 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// CRIO_SOLUTION_START_MODULE_ONE
+const form = document.getElementById("form");
+const emailInput = document.getElementById("email");
+const subBtn = document.querySelector(".sub-btn");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = emailInput.value;
+  localStorage.setItem("email", email);
+
+  if (validateEmail(email)) {
+    const encodedEmail = encodeURIComponent(email);
+    window.location.href = `success.html?email=${encodedEmail}`;
+  } else {
+    const errorEm = document.querySelector("em");
+    errorEm.innerHTML = "Valid email required.";
+    emailInput.style.border = "1px solid rgb(255, 99, 71)";
+    emailInput.style.color = "rgb(255, 99, 71)";
+    emailInput.style.backgroundColor = "rgba(255,99,71,0.6)";
+  }
+});
+
+// Function to validate email
+function validateEmail(email) {
+  const regex = /^\S+@\S+\.\S+$/;
+  return regex.test(email);
+}
+// CRIO_SOLUTION_END_MODULE_ONE 
